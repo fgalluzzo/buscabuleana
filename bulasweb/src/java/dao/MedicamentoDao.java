c7c5078849d92dd936c8e9ec00f1f9ac541a13e0
expedit <expedit@2bb28b4e-d555-2c21-c8cb-abbd1ac7d4e0>
2010-08-25 00:16:03 +0000

    Tinha feito M: esqueci de dar Add nos arquivos novos

---
+++ bulasweb/src/java/dao/MedicamentoDao.java
@@ -0,0 +1,46 @@
+package bulas.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
+import bulas.bean.MedicamentoBean;
+
+public class MedicamentoDao extends AbstractDao<MedicamentoBean> {
+
+	public MedicamentoDao(EntityManager em) {
+		super(em);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public List <MedicamentoBean> findByNameAssocLike(String name, String assoc) {
+		return null;
+	}
+
+	public MedicamentoBean findByNameAssocExactly(String name, String assoc) {
+		try {
+			Query q = em.createQuery("from Medicamento where nome = :nome and associacao = :assoc");
+			q.setParameter("nome", name);
+			q.setParameter("assoc", assoc);
+			return (MedicamentoBean) q.getSingleResult();
+		}
+		catch (NoResultException e) {
+			return null;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<MedicamentoBean> findByName(String name) {
+		try {
+			Query q = em.createQuery("from Medicamento where nome = :nome");
+			q.setParameter("nome", name);
+			return (List<MedicamentoBean>) q.getResultList();
+		}
+		catch (NoResultException e) {
+			return null;
+		}
+	}
+	
+}
