02e69f0f0f0f991303b556412129ca5601cc6292
fgalluzzo <fgalluzzo@2bb28b4e-d555-2c21-c8cb-abbd1ac7d4e0>
2010-08-29 18:20:13 +0000

    Busca dos medicamentos de interacao( ainda bugada)

---
+++ bulasweb1_2/src/java/controle/InteracaoControle.java
@@ -0,0 +1,56 @@
+package controle;
+
+
+import DTO.MedicamentoDTO;
+import bean.MedicamentoBean;
+import dao.MedicamentoDao;
+import java.util.List;
+import javax.el.ValueExpression;
+import javax.faces.application.Application;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import util.PersistenceFactory;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author galluzzo
+ */
+public class InteracaoControle {
+
+    private List<MedicamentoBean> medicamentosInteracao = null;
+     private EntityManager em;
+    MedicamentoDao medDao;
+    /** Creates a new instance of InteracaoControle */   
+
+    public InteracaoControle(){
+
+        em = PersistenceFactory.createEntityManager();
+        medDao = new MedicamentoDao(em);        
+        FacesContext context = FacesContext.getCurrentInstance();
+        Application app = context.getApplication();
+        ValueExpression expression = app.getExpressionFactory().createValueExpression(context.getELContext(),
+                String.format("#{%s}", "MedicamentoDTO"), Object.class);
+        MedicamentoDTO md = (MedicamentoDTO) expression.getValue(context.getELContext());       
+        
+
+        medicamentosInteracao = medDao.findInteracaoMedicamentosa(md.getMedicamento().getAssociacao());
+
+    }
+
+    public List<MedicamentoBean> getMedicamentosInteracao() {
+        return medicamentosInteracao;
+    }
+
+    public void setMedicamentosInteracao(List<MedicamentoBean> medicamentosInteracao) {
+        this.medicamentosInteracao = medicamentosInteracao;
+    }
+
+    
+
+}
--- bulasweb1_2/src/java/dao/MedicamentoDao.java
+++ bulasweb1_2/src/java/dao/MedicamentoDao.java
@@ -11,67 +11,110 @@ import bean.MedicamentoBean;
 
 public class MedicamentoDao extends AbstractDao<MedicamentoBean> {
 
-	public MedicamentoDao(EntityManager em) {
-		super(em);
-		// TODO Auto-generated constructor stub
-	}
-	
-	public List <MedicamentoBean> findByNameAssocLike(String name, String assoc) {
-		return null;
-	}
+    public MedicamentoDao(EntityManager em) {
+        super(em);
+        // TODO Auto-generated constructor stub
+    }
 
-	public MedicamentoBean findByNameAssocExactly(String name, String assoc) {
-		try {
-			Query q = em.createQuery("from Medicamento where nome = :nome and associacao = :assoc");
-			q.setParameter("nome", name);
-			q.setParameter("assoc", assoc);
-			return (MedicamentoBean) q.getSingleResult();
-		}
-		catch (NoResultException e) {
-			return null;
-		}
-	}
+    public List<MedicamentoBean> findByNameAssocLike(String name, String assoc) {
+        return null;
+    }
 
-	@SuppressWarnings("unchecked")
-	public List<MedicamentoBean> findByName(String name) {
-		try {
-			Query q = em.createQuery("from Medicamento where nome = :nome");
-			q.setParameter("nome", name);
-			return (List<MedicamentoBean>) q.getResultList();
-		}
-		catch (NoResultException e) {
-			return null;
-		}
-	}
-	
-	
-	@SuppressWarnings("unchecked")
-	public List<MedicamentoBean> findByFarmacoAndLab(List<String> labs, List<String> farmacos) {
-		try {//f in m.farmacos and 
-			
-			if (labs.size() == 0 || farmacos.size()==0) throw new NoResultException();
-						
+    public MedicamentoBean findByNameAssocExactly(String name, String assoc) {
+        try {
+            Query q = em.createQuery("from Medicamento where nome = :nome and associacao = :assoc");
+            q.setParameter("nome", name);
+            q.setParameter("assoc", assoc);
+            return (MedicamentoBean) q.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<MedicamentoBean> findByName(String name) {
+        try {
+            Query q = em.createQuery("from Medicamento where nome = :nome");
+            q.setParameter("nome", name);
+            return (List<MedicamentoBean>) q.getResultList();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<MedicamentoBean> findByFarmacoAndLab(List<String> labs, List<String> farmacos) {
+        try {//f in m.farmacos and
+
+            if (labs.size() == 0 || farmacos.size() == 0) {
+                throw new NoResultException();
+            }
+
 //			Query q = em.createQuery("select m from Medicamento m, Farmaco f "
 //					+ "where m.laboratorio.nome in (:labs) and f.nome in (:farmacos)");
-			
-			Query q = em.createNativeQuery("select distinct m.id from laboratorio l, farmaco f, associacao ass, medicamento m "
-				+ "where l.id=m.lab_detentor_id and ass.farmaco_id=f.id and ass.medicamento_id=m.id and f.nome in (:farmacos) and l.nome in (:labs)");
-			q.setParameter("labs", labs);
-			q.setParameter("farmacos", farmacos);
-			List <Integer> mids = q.getResultList();
-			if (mids.size()==0) throw new NoResultException();
-			
-			q = em.createQuery("from Medicamento where id in (:mids)");
-			q.setParameter("mids", mids);
-			return (List<MedicamentoBean>) q.getResultList();
-		}
-		catch (NoResultException e) {
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return new ArrayList<MedicamentoBean>();
-	}
-	
+
+            Query q = em.createNativeQuery("select distinct m.id from laboratorio l, farmaco f, associacao ass, medicamento m "
+                    + "where l.id=m.lab_detentor_id and ass.farmaco_id=f.id and ass.medicamento_id=m.id and f.nome in (:farmacos) and l.nome in (:labs)");
+            q.setParameter("labs", labs);
+            q.setParameter("farmacos", farmacos);
+            List<Integer> mids = q.getResultList();
+            if (mids.size() == 0) {
+                throw new NoResultException();
+            }
+
+            q = em.createQuery("from Medicamento where id in (:mids)");
+            q.setParameter("mids", mids);
+            return (List<MedicamentoBean>) q.getResultList();
+        } catch (NoResultException e) {
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new ArrayList<MedicamentoBean>();
+    }
+
+    public List<MedicamentoBean> findInteracaoMedicamentosa(String associacao) {
+        try {//f in m.farmacos and
+
+
+
+//			Query q = em.createQuery("select m from Medicamento m, Farmaco f "
+//					+ "where m.laboratorio.nome in (:labs) and f.nome in (:farmacos)");
+
+            Query q = em.createNativeQuery("select distinct (m.id) from secao_bula sb "
+                    + "inner join conteudo_secao_bula csb on csb.secao_id = sb.id "
+                    + "inner join bula b on b.id = csb.bula_id "
+                    + "inner join medicamento m on m.id = b.medicamento_fk "
+                    + "where sb.nome_curto = 'interacao' "
+                    + "and exists(select 1 from "
+                    + "medicamento m2 inner join "
+                    + "associacao a on a.medicamento_id = m2.id "
+                    + "inner join farmaco f on f.id = a.farmaco_id "
+                    + "where LOCATE(LCASE(f.nome), LCASE(csb.texto)) <> 0 "
+                    + "and m2.associacao=(:associacao)");
+            q.setParameter("associacao", associacao);
+            List<Integer> mids = q.getResultList();
+            if (mids.size() == 0) {
+                throw new NoResultException();
+            }
+            q = em.createQuery("from Medicamento where id in (:mids)");
+            q.setParameter("mids", mids);
+            return (List<MedicamentoBean>) q.getResultList();
+        } catch (NoResultException e) {
+        } catch (Exception e) {
+            e.printStackTrace();
+
+
+
+
+        }
+
+        return new ArrayList<MedicamentoBean>();
+
+
+    }
 }
+
+
+
+
