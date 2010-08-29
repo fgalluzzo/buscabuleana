7fbc1994be1e6a6814e4bcb9a386f899cbed6279
expedit <expedit@2bb28b4e-d555-2c21-c8cb-abbd1ac7d4e0>
2010-08-29 00:40:54 +0000

    Separacao do codigo que persiste as bulas no BD e do que indexa usando Lucene. Podem ser chamados separadamente mas para indexar no Lucene é necessario já ter as bulas no BD.

--- gera_indice/src/principal/Indexar.java
+++ gera_indice/src/principal/Indexar.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -31,15 +32,14 @@ import bean.SecaoBulaBean;
 import dao.BulaDao;
 import dao.SecaoBulaDao;
 
-
 public class Indexar {
 
 	private String bulasFolder = null;
 	private String indexFolder = null;
 
-	private IndexWriter indexWriter;
-	private Map <Integer, Integer> secCount = new TreeMap<Integer, Integer>();	// K=num of sections, V=num of docs
-	
+	// K=num of sections, V=num of docs
+	private Map<Integer, Integer> secCount = new TreeMap<Integer, Integer>();
+
 	private EntityManager em;
 	private SecaoBulaDao secaoBulaDao;
 	private BulaDao bulaDao;
@@ -47,7 +47,7 @@ public class Indexar {
 	public Indexar(String bulasFolder, String indexFolder) {
 		this.bulasFolder = bulasFolder;
 		this.indexFolder = indexFolder;
-		
+
 		// JPA: EM e DAOs
 		em = PersistenceFactory.createEntityManager();
 		secaoBulaDao = new SecaoBulaDao(em);
@@ -57,23 +57,21 @@ public class Indexar {
 			secCount.put(i, 0);
 		}
 	}
-	
 
 	@Override
 	protected void finalize() throws Throwable {
 		// TODO Auto-generated method stub
 		super.finalize();
-		
+
 		em.close();
 	}
-	
 
 	private String getFileContents(File file) throws FileNotFoundException {
 
 		BufferedReader br = new BufferedReader(new FileReader(file));
-		
+
 		String line = null;
-		StringBuilder sb = new StringBuilder((int) file.length() + 16); 
+		StringBuilder sb = new StringBuilder((int) file.length() + 16);
 		try {
 			while ((line = br.readLine()) != null) {
 				sb.append(line);
@@ -83,42 +81,38 @@ public class Indexar {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return sb.toString();
 	}
 
-	public Map <String, String> indexBula(File file) throws IOException {
+	private Map<String, String> persisteBula(File file) throws IOException {
 
 		// Obtem bula a partir do codigo
 		String code = file.getName().replaceAll("^([0-9]+)[.]pdf[.]txt$", "$1");
 		BulaBean bula = bulaDao.getByCodigo(code);
-		if (bula != null) return null;
+		if (bula != null) return null;	// util para quando esta continuando um processo interrompido
 
 		String text = getFileContents(file);
 
 		// Obtem nome e texto das seçoes da bula
 		BulaParser bulaParser = new BulaParser();
-		Map <String, String> fields = bulaParser.findSections(text, bulasFolder + file.getName());
+		Map<String, String> fields = bulaParser.findSections(text, bulasFolder + file.getName());
 
-		// Cria documento do Lucene
-		Document d = new Document();
-		d.add(new Field("code", code, Field.Store.YES, Field.Index.NO));//id
-		d.add(new Field("text", text, Field.Store.NO, Field.Index.ANALYZED));//name
-		
 		// Bula Bean
 		bula = new BulaBean();
 		bula.setTexto(text);
 		bula.setCodigo(code);
 		List<ConteudoSecaoBean> secoesBean = new ArrayList<ConteudoSecaoBean>();
-		
+
 		EntityTransaction tx = em.getTransaction();
 		try {
 			tx.begin();
 
 			for (String name : fields.keySet()) {
 
-				// Pega Secao de bula 
-				SecaoBulaBean secaoBulaBean = secaoBulaDao.findByShortName(name);
+				// Pega Secao de bula
+				SecaoBulaBean secaoBulaBean = secaoBulaDao
+						.findByShortName(name);
 				if (secaoBulaBean == null) {
 					secaoBulaBean = new SecaoBulaBean();
 					secaoBulaBean.setNomeCurto(name);
@@ -131,8 +125,6 @@ public class Indexar {
 				secao.setTexto(fields.get(name));
 				secao.setSecaoBula(secaoBulaBean);
 				secoesBean.add(secao);
-
-				d.add(new Field(name, fields.get(name), Field.Store.NO, Field.Index.ANALYZED));
 			}
 
 			bula.setConteudoSecao(secoesBean);
@@ -142,31 +134,64 @@ public class Indexar {
 			tx.rollback();
 			e.printStackTrace();
 		}
-		
-		indexWriter.addDocument(d);
-
-		// Write INI to disk
-//			FileWriter fw = new FileWriter(bulasFolder + "ini/" + pdf.getName() + ".ini");
-//			for (String name : fields.keySet()) {
-//				fw.write("\n[" + name + "]\n");
-//				fw.write(fields.get(name));
-//				fw.write("\n");
-//			}
-//			fw.close();
 
 		return fields;
 	}
 
-
-	public void process() {
+	
+	public void indexaBulas() {
+		
 		try {
 			File indexf = new File(indexFolder);
 			Directory dir = new SimpleFSDirectory(indexf);
 
 			SimpleAnalyzer analyzer = new SimpleAnalyzer();
-			indexWriter = new IndexWriter(dir, analyzer, MaxFieldLength.UNLIMITED);
+			IndexWriter indexWriter = new IndexWriter(dir, analyzer, MaxFieldLength.UNLIMITED);
 
-			// Obtem lista dos arquivos texto 
+
+			Collection<Integer> ids = bulaDao.getAllAsIntList();
+			for (int id: ids) {
+
+				BulaBean bula = bulaDao.findById(BulaBean.class, id);
+				if (bula != null) {			
+					String code = bula.getCodigo();
+					String text = bula.getTexto();
+					
+					// Cria documento do Lucene
+					Document d = new Document();
+					d.add(new Field("code", code, Field.Store.YES, Field.Index.NO));// id
+					d.add(new Field("text", text, Field.Store.NO, Field.Index.ANALYZED));// name
+					
+					for (ConteudoSecaoBean section : bula.getConteudoSecao()) {
+						d.add(new Field(section.getSecaoBula().getNomeCurto(), section.getTexto(),
+								Field.Store.NO, Field.Index.ANALYZED));
+					}
+			
+					indexWriter.addDocument(d);
+				}
+			}
+
+			indexWriter.optimize();
+			indexWriter.close();
+		} catch (CorruptIndexException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (LockObtainFailedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	/**
+	 * 
+	 */
+	public void persisteBulas() {
+		try {
+			// Obtem lista dos arquivos texto
 			File bulasf = new File(bulasFolder + "/plain/");
 			File[] txtFiles = bulasf.listFiles(new FilenameFilter() {
 				@Override
@@ -175,31 +200,31 @@ public class Indexar {
 				}
 			});
 
-
-			System.out.println("Existem " + txtFiles.length + " arquivos de texto de bula para analisar"); 
+			System.out.println("Existem " + txtFiles.length
+					+ " arquivos de texto de bula para analisar");
 
 			int count = 1;
 			for (File file : txtFiles) {
-				System.out.print("Indexando doc " + (count) + " de " + txtFiles.length + ": " + file.getName() + " ... ");
+				System.out.print("Analisando doc " + (count) + " de "
+						+ txtFiles.length + ": " + file.getName() + " ... ");
 
-				Map<String, String> fields = indexBula(file);
+				Map<String, String> fields = persisteBula(file);
 				if (fields != null) {
 					secCount.put(fields.size(), secCount.get(fields.size()) + 1);
-					System.out.print(String.format("%d secoes (%4.1f%% concluido)", fields.size(), 100.0 * count / txtFiles.length));
-				}
-				else {
-					System.out.print("já indexado anteriormente.");
+					System.out.print(String.format(
+							"%d secoes (%4.1f%% concluido)", fields.size(),
+							100.0 * count / txtFiles.length));
+				} else {
+					System.out.print("já persistido anteriormente.");
 				}
 
 				count++;
 				System.out.println("");
 			}
-			indexWriter.optimize();
-			indexWriter.close();
 
 			System.out.println("Concluido. Indice criado e bulas persistidas!\n");
 
-			System.out.println(String.format("Quantidade de secoes e suas respectivas quantidades de bulas:"));
+			System.out.println("Quantidade de secoes e suas respectivas quantidades de bulas:");
 			System.out.println("secoes\tbulas");
 			for (Integer key : secCount.keySet()) {
 				System.out.println(String.format("%d\t%d", key, secCount.get(key)));
@@ -213,7 +238,6 @@ public class Indexar {
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-
 			e.printStackTrace();
 		}
 	}
