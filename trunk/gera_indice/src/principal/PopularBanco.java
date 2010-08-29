68d3aee3a3a87f40444ee472097a021eccabb390
expedit <expedit@2bb28b4e-d555-2c21-c8cb-abbd1ac7d4e0>
2010-08-29 00:40:54 +0000

    Separacao do codigo que persiste as bulas no BD e do que indexa usando Lucene. Podem ser chamados separadamente mas para indexar no Lucene é necessario já ter as bulas no BD.

--- trunk/gera_indice/src/principal/PopularBanco.java
+++ trunk/gera_indice/src/principal/PopularBanco.java
@@ -7,6 +7,9 @@ public class PopularBanco {
 	/**
 	 * Gerencia a indexacao completa.
 	 * É possivel comentar trechos que já estiverem no banco/lucene
+	 * 
+	 * É necessario rodar em etapas pois ocorre OutOfMemoryError.
+	 *  
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -18,17 +21,18 @@ public class PopularBanco {
 
 		// CSV -> BD
 		// Tabelas alteradas: farmaco, medicamento, laboratorio, associacao
-		PersistAnvisaList pb = new PersistAnvisaList();
-		String listaPath = "../bulas_teste/anvisa/medicamentos.csv";
-		pb.process(new File(listaPath));
+		//PersistAnvisaList pb = new PersistAnvisaList();
+		//String listaPath = "../bulas_teste/anvisa/medicamentos.csv";
+		//pb.process(new File(listaPath));
 
 
 		// TXT -> secoes -> BD e lucene
 		// Tabelas alteradas: bula, secao_bula, conteudo_secao_bula
-		String bulasFolder = "../bulas_teste/bulas/";
-		String indexFolder = "../indice_bulas/";
-		Indexar indexar = new Indexar(bulasFolder, indexFolder);
-		indexar.process();
+		//String bulasFolder = "../bulas_teste/bulas/";
+		//String indexFolder = "../indice_bulas/";
+		//Indexar indexar = new Indexar(bulasFolder, indexFolder);
+		//indexar.persisteBulas();
+		//indexar.indexaBulas();
 
 
 		// Associar bulas aos medicamentos
